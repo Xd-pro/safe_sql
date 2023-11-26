03d7dda5156a48bcd861c43327530bd4a9bb0644
Finnbar McCloskey <xfinnbar@gmail.com>
2023-11-26 21:09:05 +0000

    thread pool

--- base.php
+++ base.php
@@ -1,142 +1,65 @@
 <?php
 
+namespace Finnbar\Duels\database;
+
 use pmmp\thread\Thread;
 use pmmp\thread\ThreadSafe;
 use pmmp\thread\ThreadSafeArray;
-use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
 
-abstract class TransactionBase {
+abstract class TransactionBase
+{
 
     public function __construct(public \PDO $db)
     {
         $db->beginTransaction();
     }
 
-    public function commit(): bool {
+    public function commit(): bool
+    {
         return $this->db->commit();
     }
 
-    public function rollBack(): bool {
+    public function rollBack(): bool
+    {
         return $this->db->rollBack();
     }
-
 }
 
-if (extension_loaded("pmmpthread")) {
-    abstract class AsyncTransaction extends ThreadSafe {
+abstract class AsyncTransaction extends ThreadSafe
+{
 
-        abstract public function run(Transaction $t);
-    }
-    
-    class DatabaseThreadInternal extends Thread {
-    
-        /** @var ThreadSafeArray<int, DataEntry> */
-        public $data;
-    
-        public function __construct(private string $databaseConnector)
-        {
-            $this->data = new ThreadSafeArray;
-        }
-    
-        public function run(): void {
-            $conn = new \PDO($this->databaseConnector);
-            while (true) {
-                /** @var DataEntry[] */
-                $toProcess = [];
-                $this->synchronized(function() use (&$toProcess) {
-                    $toPutBack = [];
-                    foreach ($this->data as $data) {
-                        if ($data->query) {
-                            $toProcess[]=$data;
-                        } else {
-                            $toPutBack[]=$data;
-                        }
-    
-                    }
-                    $this->data = ThreadSafeArray::fromArray($toPutBack);
-                    
-                });
-                /** @var DataEntry[] */
-                $toAdd = [];
-                
-                foreach ($toProcess as $data) {
-                    /** @var AsyncTransaction */
-                    $at = $data->data;
-                    $toAdd[]= new DataEntry(false, serialize($at->run(new Transaction($conn))), $data->callbackId);
-                    
-                    if ($conn->inTransaction()) {
-                        $conn->rollBack();
-                        throw new \Exception("Async transaction was not closed! Call \$transaction->commit() or \$transaction->rollBack() in the AsyncTransaction. Rolled back transaction to prevent damage");
-                    }
-                }
-                $this->synchronized(function() use (&$toAdd) {
-                    foreach ($toAdd as $ta) {
-                        $this->data[]= $ta;
-                    }
-                });
-                usleep(100000);
-            }
-        }
-    
+    abstract public function run(Transaction $t);
+}
+
+class DatabaseThread extends Thread
+{
+
+    /** @var ThreadSafeArray<int, DataEntry> */
+    public $data;
+
+    public int $active = 0;
+
+    public function __construct(private string $databaseConnector)
+    {
+        $this->data = new ThreadSafeArray;
     }
 
-    class DatabaseThread {
-
-        private DatabaseThreadInternal $thread;
-
-        public function run(AsyncTransaction $query, \Closure $onDone = null) {
-            if ($onDone === null) {
-                $onDone = function() {};
-            }
-            $id = 0;
-            while (isset(ClosureStore::$closures[$id])) {
-                $id++;
-            }
-            ClosureStore::$closures[$id] = $onDone;
-            $this->thread->synchronized(function() use (&$query, &$id) {
-                $this->thread->data[]= new DataEntry(true, $query, $id);
-            });
-        }
-
-        public function __construct(string $connectionString = null, DatabaseThreadInternal $internal = null)
-        {
-            if ($internal === null) {
-                $internal = new DatabaseThreadInternal($connectionString);
-            }
-            $this->thread = $internal;
-        }
-
-    }
-    
-    class DataEntry extends ThreadSafe {
-    
-        public function __construct(public bool $query, public $data, public ?string $callbackId = null)
-        {
-            
-        }
-    
-    }
-    
-    class ClosureStore {
-        public static array $closures = [];
-    }
-    
-    function tick(DatabaseThreadInternal $t) {
+    public function tick()
+    {
         /** @var DataEntry[] */
         $toProcess = [];
-        $t->synchronized(function() use (&$toProcess, &$t) {
+        $this->synchronized(function () use (&$toProcess) {
             $toPutBack = [];
-            foreach ($t->data as $data) {
+            foreach ($this->data as $data) {
                 if (!$data->query) {
-                    $toProcess[]=$data;
+                    $toProcess[] = $data;
                 } else {
-                    $toPutBack[]=$data;
+                    $toPutBack[] = $data;
                 }
-    
             }
-            $t->data = ThreadSafeArray::fromArray($toPutBack);
+            $this->data = ThreadSafeArray::fromArray($toPutBack);
         });
         foreach ($toProcess as $data) {
             if ($data->callbackId !== null) {
@@ -144,15 +67,132 @@ if (extension_loaded("pmmpthread")) {
                 unset(ClosureStore::$closures[$data->callbackId]);
             }
         }
-        usleep(100000);
     }
 
-    function bootstrapPocketmine(PluginBase $plugin, string $connectionString, int $pollTicks = 2): DatabaseThread {
-        $t = new DatabaseThreadInternal($connectionString);
-        $t->start(DatabaseThreadInternal::INHERIT_CLASSES);
-        $plugin->getScheduler()->scheduleRepeatingTask(new ClosureTask(function() use (&$t) {
-            tick($t);
+    public function run(): void
+    {
+        $conn = new \PDO($this->databaseConnector);
+        while (true) {
+            /** @var DataEntry[] */
+            $toProcess = [];
+            $this->synchronized(function () use (&$toProcess) {
+                $toPutBack = [];
+                foreach ($this->data as $data) {
+                    if ($data->query) {
+                        $toProcess[] = $data;
+                    } else {
+                        $toPutBack[] = $data;
+                    }
+                }
+                $this->data = ThreadSafeArray::fromArray($toPutBack);
+                $this->active = count($toProcess);
+            });
+
+            /** @var DataEntry[] */
+            $toAdd = [];
+
+            foreach ($toProcess as $data) {
+                /** @var AsyncTransaction */
+                $at = $data->data;
+                $toAdd[] = new DataEntry(false, serialize($at->run(new Transaction($conn))), $data->callbackId);
+                $this->synchronized(function () {
+                    $this->active--;
+                });
+                if ($conn->inTransaction()) {
+                    $conn->rollBack();
+                    throw new \Exception("Async transaction was not closed! Call \$transaction->commit() or \$transaction->rollBack() in the AsyncTransaction. Rolled back transaction to prevent damage");
+                }
+            }
+            $this->synchronized(function () use (&$toAdd) {
+                foreach ($toAdd as $ta) {
+                    $this->data[] = $ta;
+                }
+            });
+            usleep(100);
+        }
+    }
+}
+
+class DatabasePool
+{
+
+    /** @var DatabaseThread[] $threads */
+    private array $threads = [];
+
+    public function run(AsyncTransaction $query, \Closure $onDone = null)
+    {
+        if ($onDone === null) {
+            $onDone = function () {
+            };
+        }
+        $id = 0;
+        while (isset(ClosureStore::$closures[$id])) {
+            $id++;
+        }
+        ClosureStore::$closures[$id] = $onDone;
+        $thread = $this->strongest_thread();
+        $thread->synchronized(function () use (&$query, &$id, &$thread) {
+            $thread->data[] = new DataEntry(true, $query, $id);
+        });
+    }
+
+    public function tick()
+    {
+        foreach ($this->threads as $thread) {
+            $thread->tick();
+        }
+    }
+
+    private function strongest_thread(): DatabaseThread
+    {
+        $lowest = \PHP_INT_MAX;
+        $lowestThread = null;
+        foreach ($this->threads as $thread) {
+            if ($thread->active < $lowest) {
+                $lowest = $thread->active;
+                $lowestThread = $thread;
+            }
+        }
+        if ($lowestThread === null) throw new \Exception("No threads available to process asynchronous query");
+        return $lowestThread;
+    }
+
+    public function __construct(string $connectionString, int $workers = 1)
+    {
+        while ($workers > 0) {
+            $workers--;
+            $thread = new DatabaseThread($connectionString);
+            $thread->start(DatabaseThread::INHERIT_CLASSES);
+            $this->threads[] = $thread;
+        }
+    }
+}
+
+class DataEntry extends ThreadSafe
+{
+
+    public function __construct(public bool $query, public $data, public ?string $callbackId = null)
+    {
+    }
+}
+
+class ClosureStore
+{
+    public static array $closures = [];
+}
+
+class SafeSql
+{
+    private function __construct()
+    {
+    }
+
+    public static function bootstrapPocketmine(PluginBase $plugin, string $connectionString, int $pollTicks = 4, int $workers = 1): DatabasePool
+    {
+        $p = new DatabasePool($connectionString, $workers);
+        $plugin->getScheduler()->scheduleRepeatingTask(new ClosureTask(function () use (&$p) {
+            $p->tick();
         }), $pollTicks);
-        return new DatabaseThread(null, $t);
+        return $p;
     }
 }
